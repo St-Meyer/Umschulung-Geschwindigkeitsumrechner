0d659605fd71a6f8621f94f1fd9c13c3fa8efe67
Meyer <Meyer@L10692>
2024-05-24 10:20:58 +0200

    Projektdateien hinzufügen.

---
+++ 2024-05-24/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2024_05_24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Settings
+            Console.OutputEncoding = Encoding.UTF8;
+
+            // Deklaration
+            int input;
+            double conversation_factor, output;
+
+            // Initalisierung
+            conversation_factor = 1000.0 / (60 * 60); // Von km/h zu m/s.
+
+            // Eingabe
+            Console.Write("Geschwindigkeit [km/h]: ");
+            input = int.Parse(Console.ReadLine());
+
+            // Berechnung
+            output = input * conversation_factor;
+
+            // Ausgabe
+            Console.WriteLine("Umrechnungsfaktor: {0}", conversation_factor);
+            Console.WriteLine("Die Geschwindigkeit {0}km/h ergibt umgerechnet "+ 
+                              "in Meter pro Sekunde {1}m/s", input, output);
+        }
+    }
+}
